72635c8ab9d2dfe360bafbb9296f8b16e4a434e8
Kevin Ullmann <kev.ullmann@gmail.com>
2016-08-26 11:07:07 -0400

    draw something on the canvas

--- app/assets/javascripts/automata.js
+++ app/assets/javascripts/automata.js
@@ -1,8 +1,4 @@
 function startGame() {
-    alert("got here");
-    myGamePiece = new component(30, 30, "red", 10, 120);
-    myGamePiece.gravity = 0.05;
-    myScore = new component("30px", "Consolas", "black", 280, 40, "text");
     myGameArea.start();
 }
 
