4329eae97b73e7ebbc6ad52bde939113c50544c1
Kevin Ullmann <kev.ullmann@gmail.com>
2016-08-26 11:17:30 -0400

    draw a square on the screen

--- app/assets/javascripts/automata.js
+++ app/assets/javascripts/automata.js
@@ -1,4 +1,7 @@
+var myGamePiece;
+
 function startGame() {
+    myGamePiece = new component("red", 1, 2);
     myGameArea.start();
 }
 
@@ -9,10 +12,26 @@ var myGameArea = {
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
+}
+
+function component(color, row, col) {
+    this.width = 20;
+    this.height = 20;
+    this.row = row;
+    this.col = col;
+    this.update = function() {
+        ctx = myGameArea.context;
+	ctx.fillStyle = color;
+	ctx.fillRect(this.row * 20, this.col * 20, this.width, this.height);
+    }
+}
+
+function updateGameArea() {
+    myGamePiece.update()
 }
 
