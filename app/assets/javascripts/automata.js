8f770d987a6013f5f80c08b1ca345b2ef21bc45f
Kevin Ullmann <kev.ullmann@gmail.com>
2016-08-26 11:36:13 -0400

    drawing a checkerboard

--- app/assets/javascripts/automata.js
+++ app/assets/javascripts/automata.js
@@ -1,37 +1,45 @@
-var myGamePiece;
+var width = 20;
+var height = 20;
+var board = [];
 
 function startGame() {
-    myGamePiece = new component("red", 1, 2);
+    initBoard()
     myGameArea.start();
+}
+
+function initBoard() {
+    for (row = 0; row < 10; row++) {
+        var r = []
+        for (col = 0; col < 10; col++) {
+	    var color = (col % 2 == row % 2) ? "red" : "blue";
+            r.push(color)
+        }
+        board.push(r)
+    }
 }
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 1000;
+        this.canvas.height = 2000;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
+        this.interval = setInterval(updateGameArea, 1000);
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
-function component(color, row, col) {
-    this.width = 20;
-    this.height = 20;
-    this.row = row;
-    this.col = col;
-    this.update = function() {
-        ctx = myGameArea.context;
-	ctx.fillStyle = color;
-	ctx.fillRect(this.row * 20, this.col * 20, this.width, this.height);
-    }
-}
-
 function updateGameArea() {
-    myGamePiece.update()
+    ctx = myGameArea.context;
+    for (row = 0; row < 10; row++) {
+        for (col = 0; col < 10; col++) {
+	    var color = board[row][col]
+	    ctx.fillStyle = color;
+	    ctx.fillRect(col * width, row * height, width, height);
+      }
+   }
 }
 
