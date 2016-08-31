1bbd3f1136bf5d169a99238902b58f018bd591f5
Kevin Ullmann <kev.ullmann@gmail.com>
2016-08-31 18:55:28 -0400

    add my college paper pdf

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,9 @@ class StaticPagesController < ApplicationController
 
   def automata
   end
+
+  def pdf
+     pdf_filename = File.join(Rails.root, "app/assets/images/type_calculus.pdf")
+     send_file(pdf_filename, :filename => "type_calculus.pdf", :disposition => 'inline', :type => "application/pdf")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get  '/automata' => redirect('static_pages/automata')
   get  'static_pages/automata'
   get  'static_pages/help'
+  get  'static_pages/pdf'
 
   root 'static_pages#home'
 end
