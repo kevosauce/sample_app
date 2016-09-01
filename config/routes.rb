8a55f1d6946b43cc885e1a3aeda274e5b64bb1dc
Kevin Ullmann <kev.ullmann@gmail.com>
2016-09-01 12:08:02 -0400

    add poem to site

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get  'static_pages/automata'
   get  'static_pages/help'
   get  'static_pages/pdf'
+  get  'static_pages/nothing-poem'
 
   root 'static_pages#home'
 end
