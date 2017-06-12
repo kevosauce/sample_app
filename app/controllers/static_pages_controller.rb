1eda099c933fbaf4ad11a7c9e20381991b62c3ba
Kevin <kev.ullmann@gmail.com>
2017-06-12 10:16:17 -0500

    Add proton paper and thesis. Update homepage (#14)
    - Add link in fun stuff section to thesis
    - Add link in fun stuff section to proton paper
    - Add link to YouTube vid of paragliding stall
    - Remove sentence in About

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,12 +5,23 @@ class StaticPagesController < ApplicationController
   def automata
   end
 
-  def pdf
+  def nothing_poem
+  end
+
+  def functional_prog
      pdf_filename = File.join(Rails.root, "app/assets/images/type_calculus.pdf")
      send_file(pdf_filename, :filename => "type_calculus.pdf", :disposition => 'inline', :type => "application/pdf")
   end
 
-  def nothing_poem
+  def proton
+     pdf_filename = File.join(Rails.root, "app/assets/images/ullmann discovery of the proton.pdf")
+     send_file(pdf_filename, :filename => "ullmann discovery of the proton.pdf", :disposition => 'inline', :type => "application/pdf")
   end
+
+  def thesis
+     pdf_filename = File.join(Rails.root, "app/assets/images/ullmann ms thesis.pdf")
+     send_file(pdf_filename, :filename => "ullmann ms thesis.pdf", :disposition => 'inline', :type => "application/pdf")
+  end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get  '/automata' => redirect('static_pages/automata')
   get  'static_pages/automata'
   get  'static_pages/help'
-  get  'static_pages/pdf'
+  get  'static_pages/functional_prog'
   get  'static_pages/nothing-poem'
+  get  'static_pages/thesis'
+  get  'static_pages/proton'
 
   root 'static_pages#home'
 end
